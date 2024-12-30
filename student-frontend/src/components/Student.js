fa03e8961c945b7e731551d999b0c986d5600c97
riteelama <riteelama@gmail.com>
2024-12-30 23:43:43 +0545

    Add -Frontend Section

---
+++ student-frontend/src/components/Student.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button, Container, Paper } from '@mui/material';
+
+export default function Student() {
+	const paperStyle = {padding:'50px 20px', width:600, margin:'20px auto'}
+	const[name, setName] = useState('');
+	const[address,setAddress] = useState('');
+
+  return (
+	<Container>
+		<Paper elevation={3} style={paperStyle}>
+			<h1 style={{color:"blue"}}><u>Add Student</u></h1>
+			<form noValidate autoComplete='off'>
+				<Box
+				component="form"
+				sx={{ '& > :not(style)': { m: 1} }}
+				noValidate
+				autoComplete="off"
+				>
+				<TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
+				value={name}
+				onChange={(e)=>setName(e.target.value)}
+				/>
+				<TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth
+				value={address}
+				onChange={(e)=>setAddress(e.target.value)}
+				/>
+				<Button variant="contained" color='secondary'>
+					Submit
+				</Button>
+				</Box>
+			</form>
+		</Paper>
+	</Container>
+  );
+}
